9ec39f4f41b1d3352716a797c3975955c8240d13
Veronica Jennings <vj519021@ohio.edu>
2022-09-09 18:09:32 -0400

    git lab program in git lab 2

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+    File: git-lab-program.cc
+    Author: Veronica Jennings
+    Date: September 9, 2022
+    Description: This code tell the user my name
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+cout <<"Hello Git, from Veronica J"<< endl;
+return 0;
+}//main 
